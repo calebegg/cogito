b03f378b1a2d96fc240bae80f9b47262b50ad915
calebegg <calebegg@gmail.com>
2024-07-16 13:46:03 +0000

    Try to fix deno fresh, again

--- www/main.ts
+++ www/main.ts
@@ -12,7 +12,7 @@
 
 import '$std/dotenv/load.ts';
 
-import { start } from 'https://deno.land/x/fresh@1.6.8/server.ts';
+import { start } from '$fresh/server.ts';
 import manifest from './fresh.gen.ts';
 import config from './fresh.config.ts';
 
